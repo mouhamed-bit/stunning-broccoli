a0eee1778e945da2cc8b754ab4be65fe59c8f65c
mouhamed-bit <71283731+mouhamed-bit@users.noreply.github.com>
2020-11-22 12:45:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var wall; 
+var bullet;
+var speed, thickness, weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet=createSprite(50, 200, 50, 20);
+  bullet.velocityX=speed;
+
+  wall=createSprite(1200, 200, thickness, 200);
+}
+
+function draw() {
+  background(0);  
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage = 0.5 * weight * speed * speed /(thickness * thickness * thickness);
+  
+    if(damage>10){
+      wall.shapeColor=color("green");
+    }
+  
+    if(damage<10){
+      wall.shapeColor=color("red");
+    }
+  }
+ 
+  
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x + lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
+
